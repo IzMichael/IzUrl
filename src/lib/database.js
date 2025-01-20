8fc976354d7a2d583cbab6cf580455675a30d94b
IzMichael <michael@izmichael.com>
2025-01-20 22:11:09 +1300

    Fix env var syntax

--- src/lib/database.js
+++ src/lib/database.js
@@ -1,7 +1,8 @@
 import { writable } from 'svelte/store';
+import { env } from '$env/dynamic/private';
 import PocketBase from 'pocketbase';
 
-export const pb = new PocketBase(process.env.PB_URL);
+export const pb = new PocketBase(env.PB_URL);
 pb.autoCancellation(false);
 export const auth = writable(pb.authStore.model);
 pb.authStore.onChange(() => {
